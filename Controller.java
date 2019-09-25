13ba9c00ff224478733c2ba1d8c58bf0a58d2eff
Eefkje-Femke <40299927+Eefkje-Femke@users.noreply.github.com>
2019-09-25 19:23:15 +0200

    Java4oefenopdrachten
    Java 4 oefenopdrachten

---
+++ Controller.java
@@ -0,0 +1,36 @@
+
+import java.awt.event.ActionEvent; 
+import java.awt.event.ActionListener; 
+import java.beans.PropertyChangeEvent; 
+import java.beans.PropertyChangeListener;
+import javax.swing.*; 
+
+public class Controller extends JPanel  implements PropertyChangeListener{
+	
+    JButton clickButton;     
+    private View view;     
+    private Model model; 
+    
+    public Controller(){         
+    	model = new Model(); 
+    	model.addPropertyChangeListener(this); 
+    	view = new View();         
+    	this.add(view); 
+    
+	    clickButton = new JButton("+1");         
+	    clickButton.addActionListener(new ActionListener() {             
+	    	@Override             
+	    	public void actionPerformed(ActionEvent e) {                 
+	    		model.increase();                 
+//	    		view.setDisplay(Integer.toString(model.getCounter()));             
+	    		}         
+	    	});         
+	    this.add(clickButton);     
+    } 
+    @Override     
+    public void propertyChange(PropertyChangeEvent evt) {         
+    	 if (evt.getPropertyName().equals("counter")) {             
+    		 view.setDisplay(evt.getNewValue().toString());         
+	 }        
+	}
+}
---
+++ Oefenopdracht1.java
@@ -0,0 +1,15 @@
+
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+
+public class Oefenopdracht1 {
+	
+    public static void main(String[] args){         
+    	JFrame frame = new JFrame();         
+    	frame.setVisible(true);         
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);         
+    	frame.setSize(200,200);         
+    	frame.setLayout(new FlowLayout());         
+    	frame.add(new Controller());     
+    } 
+}
